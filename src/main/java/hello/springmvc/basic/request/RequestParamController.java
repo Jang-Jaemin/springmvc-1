08f27d08625ff0d5c4c5d7d1a26a007acb10050e
admin <wkwovlf12@naver.com>
2022-08-19 00:04:15 +0900

    22.8.19

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -115,14 +117,44 @@ import java.util.Map;
     public String requestParamMap(@RequestParam Map<String, Object> paramMap) {     log.info("username={}, age={}", paramMap.get("username"),
             paramMap.get("age"));
         return "ok";
-    }
-
     //  파라미터를 Map, MultiValueMap으로 조회할 수 있다. @RequestParam Map,
     //  Map(key=value)
     //  @RequestParam MultiValueMap
     //  MultiValueMap(key=[value1, value2, ...] ex) (key=userIds, value=[id1, id2])
     //  파라미터의 값이 1개가 확실하다면 Map을 사용해도 되지만, 그렇지 않다면 MultiValueMap을 사용 하자.
 
+    }
+
+        /** @ModelAttribute 적용 - modelAttributeV1
+         * @ModelAttribute 사용
+         * 참고: model.addAttribute(helloData) 코드도 함께 자동 적용됨, 뒤에 model을 설명할 때 자세히 설명
+         */
+        @ResponseBody
+        @RequestMapping("/model-attribute-v1")
+        public String modelAttributeV1(@ModelAttribute HelloData helloData) {     log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+            return "ok";
+
+            //  마치 마법처럼 HelloData 객체가 생성되고, 요청 파라미터의 값도 모두 들어가 있다. 스프링MVC는 @ModelAttribute가 있으면 다음을 실행한다.
+            //  HelloData 객체를생성한다.
+            //  요청 파라미터의 이름으로 HelloData 객체의 프로퍼티를 찾는다. 그리고 해당 프로퍼티의 setter를 호출해서 파라미터의 값을 입력(바인딩) 한다.
+            //  예) 파라미터 이름이 username이면 setUsername() 메서드를 찾아서 호출하면서 값을 입력한다.
+        }
+
+        /** @ModelAttribute 생략 - modelAttributeV2
+         * @ModelAttribute 생략 가능
+         * String, int 같은 단순 타입 = @RequestParam
+         * argument resolver 로 지정해둔 타입 외 = @ModelAttribute  */
+        @ResponseBody
+        @RequestMapping("/model-attribute-v2")
+        public String modelAttributeV2(HelloData helloData) {
+            log.info("username={}, age={}", helloData.getUsername(), helloData.getAge());
+            return "ok";
+            //  @ModelAttribute는 생략할 수 있다.
+            //  그런데 @RequestParam도 생략할 수 있으니 혼란이 발생할 수 있다.
+            //  스프링은 해당 생략시 다음과 같은 규칙을 적용한다.
+            //  String, int, Integer 같은단순타입 = @RequestParam
+            //  나머지 = @ModelAttribute (argument resolver 로지정해둔타입외)
+        }
 }
 
 //  request.getParameter()
